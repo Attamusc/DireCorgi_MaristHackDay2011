9fd15224d9df0efbda337cc07ba9cd5d33822f8f
Sean Dunn <sean@attamusc.com>
2011-11-19 04:23:37 -0500

    Votes for the win

--- test/UserTest.java
+++ test/UserTest.java
@@ -3,6 +3,7 @@ import java.util.*;
 import java.util.List;
 import play.test.*;
 import models.*;
+import play.libs.Crypto;
 
 public class UserTest extends UnitTest {
     
@@ -25,6 +26,23 @@ public class UserTest extends UnitTest {
         User sean = User.find("byEmail", "sean@attamusc.com").first();
         assertEquals(tempSchool, sean.school);
         assertEquals("Computer Science", sean.school.name);
+    }
+    
+    @Test
+    public void addVoteTest() {
+        School tempSchool = new School("Computer Science").save();
+        User sean = new User("Sean", "Dunn", "sean@attamusc.com", "password", tempSchool, "101-10-951").save();
+        User ryan = new User("Ryan", "Wallner", "ryan@attamusc.com", "password", tempSchool, "101-10-950").save();
+        VotingSession spring_2012 = new VotingSession("Spring 2012", new Date(), new Date()).save();
+        
+        sean.addVote(ryan, spring_2012);
+        
+        assertEquals(sean.votes.get(0), Vote.find("byVoter", Crypto.passwordHash(sean.email)).first());
+    }
+    
+    @Test
+    public void findInSchoolTest() {
+        
     }
     
     @Test
